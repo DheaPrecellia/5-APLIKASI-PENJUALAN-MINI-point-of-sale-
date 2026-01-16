3844db220b29f8b293a60b81a39f50720a481f14
Hilda Fildhah Izza Fachriza <112294459+hildaizza@users.noreply.github.com>
2026-01-16 19:06:19 +0700

    Hapus

--- Database.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-abstract class Database<T> {
-    public abstract void tambah(T item);
-    public abstract void hapus(int id);
-    public abstract T cari(int id);
-    public abstract List<T> semuaData();
-}
